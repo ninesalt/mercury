7d8f6dcef1c45c2c60685105dd4d743b40e039d4
ninesalt <youssefswailem@gmail.com>
2019-05-15 13:39:48 +0200

    Add SubmitOrder()

--- orderbook.go
+++ orderbook.go
@@ -1,11 +1,17 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
 // TODO: move this to another file
 type Order struct {
-	Price  int
-	Amount int
-	Stock  string // should probably rename this to something more general
-	Type   string
+	Price    int
+	Amount   int
+	Stock    string // should probably rename this to something more general
+	Type     string // buy/sell
+	IsMarket bool   // if market or limit order
 }
 
 type OrderBook struct {
@@ -13,10 +19,33 @@ type OrderBook struct {
 	Bids []Order
 }
 
+func (ob *OrderBook) SubmitOrder(o Order) error {
+
+	t := o.Type
+
+	if t == "" {
+		return errors.New("Order type must be defined")
+	}
+
+	// add to bids
+	if t == "buy" {
+		ob.Bids = append(ob.Bids, o)
+	}
+
+	if t == "sell" {
+		ob.Asks = append(ob.Asks, o)
+	}
+
+	return nil
+}
+
 func (*OrderBook) CalculateSpread() int {
 	return 0
 }
 
 func main() {
-
+	o := Order{Type: "buy"}
+	book := OrderBook{}
+	book.SubmitOrder(o)
+	fmt.Println(book)
 }
