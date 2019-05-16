a40b854ebe9df9b4bdda8d1ffbaa4ef59a6da1a4
ninesalt <youssefswailem@gmail.com>
2019-05-16 23:17:11 +0200

    Add user file

---
+++ user.go
@@ -0,0 +1,38 @@
+package main
+
+// This is a very basic module that creates users and manages balances
+// as of now it doesn't have any functionality that can get aggregate balances of
+// users. This will be added later if needed. This whole module should likely also
+// save/retrieve data from a database since speed is not crucial for these specific functions.
+
+// User is the basic struct that holds user information and balances
+type User struct {
+	Username string
+	Balance  map[string]float32
+}
+
+// CreateUser creates a new user with an empty balance mapping
+func CreateUser(username string) User {
+	return User{username, make(map[string]float32)}
+}
+
+//Balances get's the specified user's balances in all stocks
+func (u *User) Balances() map[string]float32 {
+	return u.Balance
+}
+
+// AddToBalance add a specified amount of a particular stock
+// to the user's balance
+func (u *User) AddToBalance(stock string, amount float32) {
+	if amount > 0 {
+		u.Balance[stock] += amount
+	}
+}
+
+// SubtractFromBalance subtracts a specified amount of a particular stock
+// from the user's balance
+func (u *User) SubtractFromBalance(stock string, amount float32) {
+	if amount > 0 {
+		u.Balance[stock] -= amount // check for negative balances?
+	}
+}
